b93e76b784469f629e487bbab881488ca7345b77
gyojinnK <ksmug1998@gmail.com>
2023-08-15 18:59:43 +0900

    redirect 설정

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 let http = require("http");
 let fs = require("fs");
 let url = require("url");
+let qs = require("querystring");
+const path = require("path");
 
 /**
  *
@@ -20,6 +22,7 @@ const templateHTML = (title, list, body) => {
     <body>
         <h1><a href="/">WEB</a></h1>
         ${list}
+        <a href="/create">create</a>
         ${body}
     </body>
     </html>
@@ -46,11 +49,9 @@ let app = http.createServer((req, res) => {
     let _url = req.url;
     let queryData = url.parse(_url, true).query;
     let pathname = url.parse(_url, true).pathname;
-
     if (pathname === "/") {
         if (queryData.id === undefined) {
             fs.readdir("./data", (err, filelist) => {
-                console.log(filelist);
                 let title = "Welcome";
                 let description = "Hello, Node.js!";
                 let list = templateList(filelist);
@@ -61,7 +62,6 @@ let app = http.createServer((req, res) => {
             });
         } else {
             fs.readdir("./data", (err, filelist) => {
-                console.log(filelist);
                 fs.readFile(
                     `./data/${queryData.id}`,
                     "utf-8",
@@ -80,6 +80,55 @@ let app = http.createServer((req, res) => {
                 );
             });
         }
+    } else if (pathname === "/create") {
+        fs.readdir("./data", (err, filelist) => {
+            let title = "WEB - create";
+            let list = templateList(filelist);
+            res.writeHead(200); // 웹서버가 200을 준다는 것은 성공적으로 작업을 종료했다는 뜻
+            res.end(
+                templateHTML(
+                    title,
+                    list,
+                    `
+                    <form action="http://localhost:3000/create_process" method="post">
+                        <p></p><input type="text" name="title" placeholder="title"/></p>
+                        <p><textarea name='description' placeholder="input description this area!"></textarea></p>
+                        <input type="submit"/>
+                    </form>
+                    `
+                )
+            );
+        });
+    } else if (pathname === "/create_process") {
+        let body = "";
+        // res.on() = 데이터의 조각(data)을 수신할 때 마다 on의 콜백함수를 호출함
+        req.on("data", (data) => {
+            body += data;
+            if (body.length > 1e6) {
+                req.connection.destroy();
+            }
+        });
+        // post로 전송된 데이터를 담은 body를 qs.parse(qs = require('querystring))로
+        // parsing하여 post변수로 데이터 처리 가능
+        req.on("end", () => {
+            let post = qs.parse(body);
+            let title = post.title;
+            let description = post.description;
+
+            fs.writeFile(`data/${title}`, description, "utf-8", (err) => {
+                if (err) {
+                    console.log(err.message);
+                }
+                // res.writeHead(200);
+                // nodeJS에서 redirect사용법
+                // writeHead(200) -> 페이지를 성공적으로 가져옴을 뜻함
+                // writeHead(302, {Loction: specific spot}) -> Loction으로 페이지를 이동시킴
+                res.writeHead(302, {
+                    Location: `/?id=${title}`,
+                });
+                res.end("success");
+            });
+        });
     } else {
         res.writeHead(404);
         res.end("Not found");
