675974d9700cd6bf2d5d9175fb9f74315eec723b
gyojinnK <ksmug1998@gmail.com>
2023-08-16 20:43:57 +0900

    npm init

--- main.js
+++ main.js
@@ -2,6 +2,9 @@ let http = require("http");
 let fs = require("fs");
 let url = require("url");
 let qs = require("querystring");
+let template = require("./lib/template.js");
+let sanitizeHtml = require("sanitize-html");
+
 const path = require("path");
 
 /**
@@ -11,39 +14,6 @@ const path = require("path");
  * @param body 타이틀과 설명
  * @returns HTML 생성 코드
  */
-const templateHTML = (title, list, body, control) => {
-    return `
-    <!doctype html>
-    <html>
-    <head>
-        <title>WEB1 - ${title}</title>
-        <meta charset="utf-8">
-    </head>
-    <body>
-        <h1><a href="/">WEB</a></h1>
-        ${list}
-        ${control}
-        ${body}
-    </body>
-    </html>
-`;
-};
-
-/**
- *
- * @param filelist ./data dir 속 파일의 리스트
- * @returns list HTML 코드
- */
-const templateList = (filelist) => {
-    let list = "<ul>";
-    let i = 0;
-    while (i < filelist.length) {
-        list += `<li><a href='/?id=${filelist[i]}'>${filelist[i]}</a></li>`;
-        i++;
-    }
-    list += "</ul>";
-    return list;
-};
 
 let app = http.createServer((req, res) => {
     let _url = req.url;
@@ -54,10 +24,10 @@ let app = http.createServer((req, res) => {
             fs.readdir("./data", (err, filelist) => {
                 let title = "Welcome";
                 let description = "Hello, Node.js!";
-                let list = templateList(filelist);
+                let list = template.list(filelist);
                 res.writeHead(200); // 웹서버가 200을 준다는 것은 성공적으로 작업을 종료했다는 뜻
                 res.end(
-                    templateHTML(
+                    template.HTML(
                         title,
                         list,
                         `<h2>${title}</h2>${description}`,
@@ -67,22 +37,27 @@ let app = http.createServer((req, res) => {
             });
         } else {
             fs.readdir("./data", (err, filelist) => {
+                // 사용자의 입력으로부터 보안 실시.
+                // 들어보는 path의 base 값만으로 핸들링하여 보안
+                let filteredId = path.parse(queryData.id).base;
                 fs.readFile(
-                    `./data/${queryData.id}`,
+                    `./data/${filteredId}`,
                     "utf-8",
                     (err, description) => {
                         let title = queryData.id;
-                        let list = templateList(filelist);
+                        let sanitizedTitle = sanitizeHtml(title);
+                        let sanitizedDescription = sanitizeHtml(description);
+                        let list = template.list(filelist);
                         res.writeHead(200); // 웹서버가 200을 준다는 것은 성공적으로 작업을 종료했다는 뜻
                         res.end(
-                            templateHTML(
-                                title,
+                            template.HTML(
+                                sanitizedTitle,
                                 list,
-                                `<h2>${title}</h2>${description}`,
+                                `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
                                 `<a href="/create">create</a> 
-                                 <a href='/update?id=${title}'>update</a> 
+                                 <a href='/update?id=${sanitizedTitle}'>update</a> 
                                  <form action="/delete_process" method="post" onsubmit="">
-                                    <input type="hidden" name="id" value="${title}"/>
+                                    <input type="hidden" name="id" value="${sanitizedTitle}"/>
                                     <input type="submit" value="delete"/>
                                  </form>
                                 `
@@ -95,10 +70,10 @@ let app = http.createServer((req, res) => {
     } else if (pathname === "/create") {
         fs.readdir("./data", (err, filelist) => {
             let title = "WEB - create";
-            let list = templateList(filelist);
+            let list = template.list(filelist);
             res.writeHead(200); // 웹서버가 200을 준다는 것은 성공적으로 작업을 종료했다는 뜻
             res.end(
-                templateHTML(
+                template.HTML(
                     title,
                     list,
                     `
@@ -144,18 +119,16 @@ let app = http.createServer((req, res) => {
         });
     } else if (pathname === "/update") {
         fs.readdir("./data", (err, filelist) => {
-            fs.readFile(
-                `./data/${queryData.id}`,
-                "utf-8",
-                (err, description) => {
-                    let title = queryData.id;
-                    let list = templateList(filelist);
-                    res.writeHead(200); // 웹서버가 200을 준다는 것은 성공적으로 작업을 종료했다는 뜻
-                    res.end(
-                        templateHTML(
-                            title,
-                            list,
-                            `
+            let filteredId = path.parse(queryData.id).base;
+            fs.readFile(`./data/${filteredId}`, "utf-8", (err, description) => {
+                let title = queryData.id;
+                let list = template.list(filelist);
+                res.writeHead(200); // 웹서버가 200을 준다는 것은 성공적으로 작업을 종료했다는 뜻
+                res.end(
+                    template.HTML(
+                        title,
+                        list,
+                        `
                             <form action="/update_process" method="post">
                                 <input type='hidden' name='id' value='${title}'/>
                                 <p></p><input type="text" name="title" placeholder="title" value='${title}'/></p>
@@ -163,13 +136,12 @@ let app = http.createServer((req, res) => {
                                 <input type="submit"/>
                             </form>
                             `,
-                            `<a href="/create">create</a>
+                        `<a href="/create">create</a>
                              <a href='/update?id=${title}'>update</a> 
                             `
-                        )
-                    );
-                }
-            );
+                    )
+                );
+            });
         });
     } else if (pathname === "/update_process") {
         let body = "";
@@ -210,7 +182,8 @@ let app = http.createServer((req, res) => {
         req.on("end", () => {
             let post = qs.parse(body);
             let id = post.id;
-            fs.unlink(`data/${id}`, (err) => {
+            let filteredId = path.parse(id).base;
+            fs.unlink(`data/${filteredId}`, (err) => {
                 res.writeHead(302, {
                     Location: `/`,
                 });
